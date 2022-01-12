265afea0e6281536a38b72a5819672e88f89dffc
MasayLighto <www.masaylighto@gmail.com>
2022-01-12 15:18:15 +0300

    add comment and to the source code

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use std::env;
-use actix_web::{get, post,web::{self},App, HttpResponse, HttpServer, Responder};
 mod web_server;
 
 fn main() -> std::io::Result<()> {
     env::set_var("RUST_LOG", "actix_web=debug,actix_server=info");
     env_logger::init();
-    web_server::web_server::start_the_server()
+
+    // we start the serverf
+    web_server::start_the_server()
 }
--- src/web_server.rs
+++ src/web_server.rs
@@ -1,146 +1,103 @@
+ 
+use std::{fs::{self, File}, io::Read};
+use actix_multipart::{Multipart};
+use actix_web::{App, HttpServer, HttpResponse, get, Responder, post, web};
+use futures_util::TryStreamExt;
+use serde::Deserialize;
 mod file_manger;
-pub mod web_server {
-    use std::{fs::{self, File}, io::Read};
-
-    use actix_multipart::{Field, MultipartError, Multipart};
-    use actix_web::{App, HttpServer, HttpResponse, get, Responder, post, web};
-    use futures_util::TryStreamExt;
-    use serde::Deserialize;
-
-    use crate::web_server::file_manger::file_manger;
-
-    
-
+mod form_parser;
 #[derive(Deserialize)]
+/// stract that implemented for the get_image_as_byte_array get request
 struct FileInfo 
 {
     path: String,
 }
+/// this method return  buffer of bytes that represent the img to the client
 #[get("/GetImageAsByteArray")]
+// web::Query<FileInfo> paramter will parse the http request of type git into the field of the struct named FileInfo
 async fn get_image_as_byte_array(request: web::Query<FileInfo>) -> impl Responder 
 {
-    let file = File::open(&request.path);
-    if file.is_err() 
-    {
-        return HttpResponse::Ok().body(format!("File not found {}", &request.path));
-    }
-    let metadata = fs::metadata(&request.path);    
-    if metadata.is_err() 
-    {
-        return HttpResponse::Ok().body("Couldnt read File metadata");
-    }
-    let mut buffer: Vec<u8> = vec![0; metadata.unwrap().len() as usize];
-    if file.unwrap().read(&mut buffer).is_err() 
-    {
-        return HttpResponse::Ok().body("Couldnt read File Data");
-    }
-    HttpResponse::Ok().body(buffer)
+    // we use the method file_manger::read_file which read the data of img you passed it path 
+    // the return type of the methos is Result<Vec<u8>,String> 
+    // string is returned when there is error and it indicate the error and we return it to the client
+    // the vec<u8> represnt the img bytes and we return it to the client
+   match file_manger::read_file(&request.path).await
+   {
+        Ok(data)=>
+        {
+            return HttpResponse::Ok().body(data);
+        },
+        Err(err_msg) => 
+        {
+            return HttpResponse::Ok().body(err_msg);
+        },
+   }
+
 }
+
+/// Save Image that Posted by client with  method="post" enctype="multipart/form-data" 
+/// the post should contain three field the first one is the directory you want to store the img into it
+/// second the img name third the img bytes 
 #[post("/SaveImage")]
+// the multipart parameter represnt the form buffer and you can get the data from it one by ine throw the method try_next
 async fn save_image(mut payload: Multipart) -> impl Responder 
 {  
-    let folder_name:String = match get_string(payload.try_next().await).await 
+
+    // here we use the form_parser::get_string method which is responsible to get a string out of the post field we pass into it the first field using try next method 
+    // if you are wandering why we use await method ,that is because all the method in this app are async method
+    // the get strng return Option<String> and that why we used Match pattern on it to extract the data
+    let folder_name:String = match form_parser::get_string(payload.try_next().await).await 
     {
         Some(data)=> data,
-        None=> return HttpResponse::Ok().body("Fail to Field Parse  First Result Field in the form")
+        None=> return HttpResponse::Ok().body("Fail to Parse First Field in the Post")
     };
-
-    let file_name:String = match get_string(payload.try_next().await).await 
+    // we apply the same method we mention above again but this time for the next field of recived post which represent the file name 
+    let file_name:String = match form_parser::get_string(payload.try_next().await).await 
     {
         Some(data)=> data,
-        None=> return HttpResponse::Ok().body("Fail to Field Parse  First Result Field in the form")
+        None=> return HttpResponse::Ok().body("Fail to Parse Second Field in the Post")
     };  
-
-    let content:Vec<u8> = match  get_bytes(payload.try_next().await).await
+    // here we apply the method form_parser::get_bytes to get the img buffer form the third post 
+    // the return type of this method is Option<Vec<u8>> and that why we used the match pattern to extract the data    
+    let img_buffer:Vec<u8> = match  form_parser::get_bytes(payload.try_next().await).await
     {
+        // if the data is present in the option we assigin it to the img_buffer 
         Some(data)=> data,
-        None=> return HttpResponse::Ok().body("Fail to Field Parse  First Result Field in the form")
+        // if there is no data present we end the excution and retun failed message to the used
+        None=> return HttpResponse::Ok().body("Fail tp Parse third Field in the Post")
     }; 
-
-    if file_manger::save_file(file_name,folder_name,content).await
+    // here we used the file_manger::create_file to create file with the information we extracted above
+    // the method return true if it successed and false other wise
+    // we check the return value if its true we return Done to the used if it not we return Fail
+    if file_manger::create_file(file_name,folder_name,img_buffer).await
     {
         return HttpResponse::Ok().body("Done");
     }
     return HttpResponse::Ok().body("Fail");
 }
-async fn get_string(payload:Result<Option<Field>,MultipartError>)->Option<String>
-{
-    let name_bytes=match  get_bytes(payload).await 
-    {
-        Some(data)=> data,
-        None=>{return None;}
-    };   
-
-    let data = format!("{}", String::from_utf8_lossy(&name_bytes));
-    Some(data)
-}
-async fn get_bytes(field:Result<Option<Field>,MultipartError>)->Option<Vec<u8>>
-{
-    let field = match field
-    {
-        Ok(data) =>data,        
-        Err(_) => 
-        {
-            println!("Fail to Parse Payload First Result Field in the form {}",line!() );
-            return None;
-        }
-    };
-    get_field_bytes(field).await  
-}
-async fn get_field_bytes(field:Option<Field>)->Option<Vec<u8>>
-{
-    let mut field = match field
-    {
-        Some(data) => data,
-        
-        None =>
-        {
-            println!("Fail to Parse Payload  First Option Field in the form {}",line!() );
-            return None;
-        }
-    };
-    let mut bytes_vec=Vec::<u8>::new();
-    let mut itteration_count=0;
-    while let Ok(data) = field.try_next().await 
-    {
-        let field=match data  
-        {
-            Some(data) =>data,
-            None => 
-            { 
-                if itteration_count>0 
-                {                    
-                    break
-                }      
-                println!("Fail to Parse Payload First Result Field in the form {}",line!() );   
-                return  None;   
-            }
-        };
-        itteration_count+=1;
-        bytes_vec.append(&mut field.to_vec());
-    }
-   Some(bytes_vec)
-}
-
+/// Create an Index Page
 #[get("/")]
-fn index() -> HttpResponse {
-  
+fn index() -> HttpResponse 
+{ 
+    // Get Page Content form file during completion and store it into the app using include_str
     let html = include_str!("../Form.html");
+    // return the page content into the client
     HttpResponse::Ok().body(html)
 }
+/// Run the web server
 #[actix_web::main]
 pub async fn start_the_server()-> std::io::Result<()>
 {
-
+    // start new instance form the server and set the main there api endpoint method 
     HttpServer::new(|| {
         App::new()
             .service(get_image_as_byte_array)
             .service(save_image)
             .service(index)
     })
+    // bind the server into the specifed ip address
     .bind("127.0.0.1:8083")?
     .run()
     .await
 
-}
 }
